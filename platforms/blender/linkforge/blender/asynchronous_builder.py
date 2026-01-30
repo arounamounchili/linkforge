18cab05b2d5b7bffa763b42e5b59b3df67e854d6
arounamounchili <virtual.power.virtual@gmail.com>
2026-01-30 04:26:09 +0100

    fix: restore required imports in asynchronous builder after refactor

--- platforms/blender/linkforge/blender/asynchronous_builder.py
+++ platforms/blender/linkforge/blender/asynchronous_builder.py
@@ -14,6 +14,9 @@ import bpy
 from ..linkforge_core.logging_config import get_logger
 from ..linkforge_core.models import Robot
 from .scene_builder import (
+    create_joint_object,
+    create_link_object,
+    create_sensor_object,
     setup_scene_for_robot,
 )
 from .utils.kinematics import sort_joints_topological
