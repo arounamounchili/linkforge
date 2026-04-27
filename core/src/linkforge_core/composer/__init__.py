7d0331b8f324849113339479dec9bb93183c96c0
arounamounchili <patouossa.mounchili@gmail.com>
2026-04-27 22:05:23 +0200

    refactor: remove legacy factory functions and streamline composer exports
    Signed-off-by: arounamounchili <patouossa.mounchili@gmail.com>

--- core/src/linkforge_core/composer/__init__.py
+++ core/src/linkforge_core/composer/__init__.py
@@ -1,10 +1,9 @@
 """Robot assembly and composition tools.
 
 This package provides high-level APIs for building modular robots
-by attaching components and programmatically constructing links/joints.
+by programmatically constructing links, joints, and semantic data.
 """
 
-from .factories import fixed_joint, origin, revolute_joint
 from .robot_builder import RobotBuilder
 
-__all__ = ["RobotBuilder", "fixed_joint", "revolute_joint", "origin"]
+__all__ = ["RobotBuilder"]
